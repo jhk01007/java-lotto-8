d6e94d64d098912cdd5123d81dcc91c7cf7939cf
jhk01007 <jhk001007@naver.com>
2025-10-30 12:54:52 +0900

    feat(ComputeLottoStatisticsService): 로또 결과 통계계산 서비스 로직 구현
    - 해당 통계는 구매한 로또 티켓, 당첨 번호를 이용한 통계성 데이터이고 현재 요구사항상 이 통계 데이터를 출력만하기 때문에 별도의 도메인 모델보다는 서비스로 분리하는 것이 적절하다고 판단

---
+++ src/main/java/lotto/service/ComputeLottoStatisticsService.java
@@ -0,0 +1,107 @@
+package lotto.service;
+
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoWinningNumbers;
+import lotto.domain.vo.Lotto;
+import lotto.domain.vo.LottoRank;
+import lotto.domain.vo.PurchaseAmount;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class ComputeLottoStatisticsService {
+
+    public HashMap<LottoRank, Integer> computeWinningResults(
+            LottoTicket lottoTicket, LottoWinningNumbers lottoWinningNumbers) {
+        HashMap<LottoRank, Integer> results = initWinningResults();
+        // 구매한 각 로또에 대해 당첨번호 매칭
+        for (Lotto lotto : lottoTicket.getPurchasedLottos()) {
+            HashSet<Integer> lottoSet = new HashSet<>(lotto.getNumbers());
+            // 당첨번호 중 일치하는 것이 몇개인지 계산
+            int matchedCount = countMatchedNumbers(lottoSet, lottoWinningNumbers.getWinningNumbers());
+            // 보너스 번호와 일치하는지 확인
+            boolean isBonusNumberMatch = isBonusNumberMatch(lottoSet, lottoWinningNumbers.getBonusNumber());
+            // 현재 로또의 결과를 기록
+            updateWinningResults(matchedCount, isBonusNumberMatch, results);
+        }
+        return results;
+    }
+
+    public double computeProfitRate(Map<LottoRank, Integer> winningResults, PurchaseAmount purchaseAmount) {
+
+        double totalWinningAmount = 0;
+        for (LottoRank lottoRank : winningResults.keySet()) {
+            totalWinningAmount += lottoRank.getWinningAmount() * winningResults.get(lottoRank);
+        }
+
+        return totalWinningAmount / purchaseAmount.getAmount() * 100;
+    }
+
+    /*
+    private 메서드 시작
+     */
+    private static HashMap<LottoRank, Integer> initWinningResults() {
+        HashMap<LottoRank, Integer> initWinningResults = new HashMap<>(LottoRank.values().length); // 등수 갯수 만큼 Map의 크기를 제한
+        for (LottoRank lottoRank : LottoRank.values()) {
+            initWinningResults.put(lottoRank, 0);
+        }
+        return initWinningResults;
+    }
+
+    private static int countMatchedNumbers(HashSet<Integer> lottoSet, List<Integer> winningNumbers) {
+        int matchedCount = 0;
+        for (int winningNumber : winningNumbers) {
+            if (!lottoSet.add(winningNumber)) {
+                matchedCount++;
+            }
+        }
+        return matchedCount;
+    }
+
+    private static boolean isBonusNumberMatch(HashSet<Integer> lottoSet, int bonusNumber) {
+        return !lottoSet.add(bonusNumber);
+    }
+
+    private static void updateWinningResults(
+            int matchedCount,
+            boolean isBonusNumberMatch,
+            HashMap<LottoRank, Integer> results
+    ) {
+        for (LottoRank rank : LottoRank.values()) {
+            // 일치하지 않은 경우 패스
+            if (rank.getMatchedCount() != matchedCount) {
+                continue;
+            }
+            // 5개 맞춘 경우 2등, 3등 구분 필요
+            if (matchedCount == 5) {
+                handleFiveMatch(rank, isBonusNumberMatch, results);
+                return;
+            }
+            // 그외의 경우
+            increment(results, rank);
+            return;
+        }
+    }
+
+    private static void handleFiveMatch(
+            LottoRank rank,
+            boolean isBonusNumberMatch,
+            HashMap<LottoRank, Integer> results
+    ) {
+        // 2등: 보너스 일치 O
+        if (isBonusNumberMatch && rank == LottoRank.SECOND) {
+            increment(results, rank);
+            return;
+        }
+        // 3등: 보너스 일치 X
+        if (!isBonusNumberMatch && rank == LottoRank.THIRD) {
+            increment(results, rank);
+        }
+    }
+
+    private static void increment(HashMap<LottoRank, Integer> results, LottoRank rank) {
+        results.merge(rank, 1, Integer::sum);
+    }
+}
---
+++ src/test/java/lotto/service/ComputeLottoStatisticsServiceTest.java
@@ -0,0 +1,87 @@
+package lotto.service;
+
+import lotto.domain.LottoTicket;
+import lotto.domain.LottoWinningNumbers;
+import lotto.domain.vo.Lotto;
+import lotto.domain.vo.LottoRank;
+import lotto.domain.vo.PurchaseAmount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComputeLottoStatisticsServiceTest {
+
+    private final ComputeLottoStatisticsService service = new ComputeLottoStatisticsService();
+
+    @Test
+    @DisplayName("로또 티켓과 당첨 번호를 통해 당첨결과를 계산한다.")
+    public void computeWinningResults() throws Exception {
+        // given
+        LottoTicket lottoTicket = createLottoTicket(8000, List.of(
+                Lotto.from(List.of(8, 21, 23, 41, 42, 43)),
+                Lotto.from(List.of(3, 5, 11, 16, 32, 38)),
+                Lotto.from(List.of(7, 11, 16, 35, 36, 44)),
+                Lotto.from(List.of(1, 8, 11, 31, 41, 42)),
+                Lotto.from(List.of(13, 14, 16, 38, 42, 45)),
+                Lotto.from(List.of(7, 11, 30, 40, 42, 43)),
+                Lotto.from(List.of(2, 13, 22, 32, 38, 45)),
+                Lotto.from(List.of(1, 3, 5, 14, 22, 45))
+        ));
+        LottoWinningNumbers lottoWinningNumbers = createLottoWinningNumbers();
+
+        // when
+        HashMap<LottoRank, Integer> results = service.computeWinningResults(lottoTicket, lottoWinningNumbers);
+
+        // then
+        assertThat(results).hasSize(LottoRank.values().length)
+                .containsExactlyInAnyOrderEntriesOf(
+                        Map.of(
+                                LottoRank.FIRST, 0,
+                                LottoRank.SECOND, 0,
+                                LottoRank.THIRD, 0,
+                                LottoRank.FOURTH, 0,
+                                LottoRank.FIFTH, 1
+                        )
+                );
+    }
+
+    @Test
+    @DisplayName("구매 금액과 당첨결과를 통해 수익률을 계산한다.")
+    public void computeProfitRate() throws Exception {
+        // given
+        Map<LottoRank, Integer> winningResults = Map.of(
+                LottoRank.FIRST, 0,
+                LottoRank.SECOND, 0,
+                LottoRank.THIRD, 0,
+                LottoRank.FOURTH, 0,
+                LottoRank.FIFTH, 1
+        );
+        PurchaseAmount purchaseAmount = PurchaseAmount.from(8000);
+
+        // when
+        double profitRate = service.computeProfitRate(winningResults, purchaseAmount);
+
+        // then
+        assertThat(profitRate).isEqualTo(62.5);
+    }
+
+    private static LottoTicket createLottoTicket(int amount, List<Lotto> lottos) {
+        return LottoTicket.of(
+                PurchaseAmount.from(amount),
+                lottos
+        );
+    }
+
+    private static LottoWinningNumbers createLottoWinningNumbers() {
+        return LottoWinningNumbers.of(
+                List.of(1, 2, 3, 4, 5, 6),
+                7
+        );
+    }
+
+}
