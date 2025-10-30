9585dd758c36ca17d68c939889f237f84f469125
jhk01007 <jhk001007@naver.com>
2025-10-30 12:21:51 +0900

    feat(LottoTicket): 로또 티켓 도메인 모델 구현
    - 여러 개의 로또 번호와 구매금액을 포함

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import lotto.domain.vo.Lotto;
+import lotto.domain.vo.PurchaseAmount;
+
+import java.util.List;
+
+import static lotto.global.exception.ErrorMessage.INVALID_LOTTO_TICKET_ERROR;
+
+public class LottoTicket {
+
+    private final PurchaseAmount purchaseAmount;
+    private final List<Lotto> purchasedLottos;
+
+    private LottoTicket(PurchaseAmount purchaseAmount, List<Lotto> purchasedLottos) {
+        this.purchaseAmount = purchaseAmount;
+        this.purchasedLottos = purchasedLottos;
+    }
+
+    public static LottoTicket of(PurchaseAmount purchaseAmount, List<Lotto> lottoGames) {
+        validate(purchaseAmount, lottoGames);
+        return new LottoTicket(purchaseAmount, lottoGames);
+    }
+
+    private static void validate(PurchaseAmount purchaseAmount, List<Lotto> lottoGames) {
+        if(purchaseAmount.getPurchaseCount() != lottoGames.size()) {
+            throw new IllegalArgumentException(INVALID_LOTTO_TICKET_ERROR.getMessage());
+        }
+    }
+
+    public PurchaseAmount getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public List<Lotto> getPurchasedLottos() {
+        return List.copyOf(purchasedLottos);
+    }
+}
---
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+import lotto.domain.vo.Lotto;
+import lotto.domain.vo.PurchaseAmount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static lotto.global.exception.ErrorMessage.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoTicketTest {
+
+    @Test
+    @DisplayName("로또 갯수가 구매 금액 갯수에 맞아야 LottoTicket을 생성할 수 있다.")
+    public void of_success() throws Exception {
+        // given
+        PurchaseAmount purchaseAmount = PurchaseAmount.from(3000);// 로또 3개 구매
+        List<Lotto> lottos = List.of(
+                Lotto.from(List.of(1, 2, 3, 4, 5, 6)),
+                Lotto.from(List.of(1, 2, 12, 15, 18, 24)),
+                Lotto.from(List.of(5, 7, 11, 23, 35, 40))
+        );
+
+        // when
+        LottoTicket lottoTicket = LottoTicket.of(purchaseAmount, lottos);
+
+        // then
+        assertThat(lottoTicket.getPurchaseAmount()).isEqualTo(purchaseAmount);
+        assertThat(lottoTicket.getPurchasedLottos()).isEqualTo(lottos);
+    }
+
+    @Test
+    @DisplayName("로또 갯수가 구매 금액 갯수가 맞지 않으면 예외가 발생한다.")
+    public void of_fail() throws Exception {
+        // given
+        PurchaseAmount purchaseAmount = PurchaseAmount.from(3000);// 로또 3개 구매
+        List<Lotto> lottos = List.of(
+                Lotto.from(List.of(1, 2, 3, 4, 5, 6)),
+                Lotto.from(List.of(1, 2, 12, 15, 18, 24))
+        ); // 로또는 2개뿐
+
+        // when then
+        assertThatThrownBy(() -> LottoTicket.of(purchaseAmount, lottos))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_LOTTO_TICKET_ERROR.getMessage());
+    }
+
+}
