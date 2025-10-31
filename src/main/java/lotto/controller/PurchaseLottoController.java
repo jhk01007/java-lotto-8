197aa9b0582aae3e9caebe6e4d1e333c8ff89d98
jhk01007 <jhk001007@naver.com>
2025-10-31 16:42:27 +0900

    remove(LottoMapper): LottoMapper 제거
    - 클래스로의 분리의 필요성을 못느낌
    - 추후 매핑 로직이 많아지면 그때 다시 분리

--- src/main/java/lotto/controller/PurchaseLottoController.java
+++ src/main/java/lotto/controller/PurchaseLottoController.java
@@ -2,7 +2,6 @@ package lotto.controller;
 
 import lotto.controller.dto.request.PurchaseRequest;
 import lotto.controller.dto.response.PurchaseResponse;
-import lotto.controller.mapper.LottoMapper;
 import lotto.domain.LottoTicket;
 import lotto.domain.vo.PurchaseAmount;
 import lotto.global.BaseResponse;
@@ -26,6 +25,15 @@ public class PurchaseLottoController {
             return BaseResponse.onFailure(e.getMessage());
         }
 
-        return BaseResponse.onSuccess(LottoMapper.toPurchaseResponse(lottoTicket));
+        return BaseResponse.onSuccess(toPurchaseResponse(lottoTicket));
+    }
+
+    private static PurchaseResponse toPurchaseResponse(LottoTicket lottoTicket) {
+        return new PurchaseResponse(
+                lottoTicket.getPurchaseAmount().getNumberOfGames(),
+                lottoTicket.getPurchasedLottos().stream()
+                        .map(lotto -> new PurchaseResponse.LottoDto(lotto.getNumbers()))
+                        .toList()
+        );
     }
 }
