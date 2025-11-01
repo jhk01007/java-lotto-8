09905fd6d98b38fcb0630bd011dbe762024bae37
jhk01007 <jhk001007@naver.com>
2025-11-01 14:59:00 +0900

    refactor(LottoRank): winningAmount 가독성 있게 변경
    - long 타입의 _로 숫자 가독성 있게 변경

--- src/main/java/lotto/domain/vo/LottoRank.java
+++ src/main/java/lotto/domain/vo/LottoRank.java
@@ -3,11 +3,11 @@ package lotto.domain.vo;
 public enum LottoRank {
 
     // TODO: 상금이 회차별로 변경되는 부분 고려 - java.util.Properties
-    FIRST(1, 6, 2000000000),
-    SECOND(2, 5, 30000000),
-    THIRD(3, 5, 1500000),
-    FOURTH(4, 4, 50000),
-    FIFTH(5, 3, 5000);
+    FIRST(1, 6, 2_000_000_000L),
+    SECOND(2, 5, 30_000_000L),
+    THIRD(3, 5, 1_500_000L),
+    FOURTH(4, 4, 50_000L),
+    FIFTH(5, 3, 5_000L);
 
     private final int rank;
     private final int matchedCount;
